0f1c76df0ef699bfeea1e1962655d56fa1f49a4d
MartinMenev <martin.menew@gmail.com>
2022-11-20 08:14:41 +0200

    New queries

---
+++ Built-in functions/Find towns NOT starting with specific letter.sql
@@ -0,0 +1,9 @@
+USE `soft_uni`;
+
+SELECT 
+    `town_id`, `name`
+FROM
+    `towns`
+WHERE
+    SUBSTRING(`name`, 1, 1) NOT IN ('R' , 'B', 'D')
+ORDER BY `name`;
---
+++ Built-in functions/Find towns starting with specific letter.sql
@@ -0,0 +1,9 @@
+USE `soft_uni`;
+
+SELECT 
+    `town_id`, `name`
+FROM
+    `towns`
+WHERE
+    SUBSTRING(`name`, 1, 1) IN ('M' , 'K', 'B', 'E')
+ORDER BY `name`;
